31de610c619f864e77a2fd27cdc0923f9f4fde7b
Uche Ozoemena <ucheozoemena@gmail.com>
2019-07-30 07:39:44 +0100

    Make delete links functional

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import './index.css';
 
-import { getUsers } from "./api/userApi";
+import { getUsers, deleteUser } from "./api/userApi";
 
 // Populate a table of users via API call
 getUsers().then(result => {
@@ -17,4 +17,19 @@ getUsers().then(result => {
   });
 
   global.document.getElementById('users').innerHTML = usersBody;
+
+  const deleteLinks = [...global.document.getElementsByClassName('deleteUser')];
+  deleteLinks.forEach(link => {
+    link.onclick = event => {
+      const element = event.target;
+      event.preventDefault();
+
+      // delete user from db
+      deleteUser(element.attributes['data-id'].value);
+
+      // delete user from DOM
+      const row = element.parentNode.parentNode;
+      row.parentNode.removeChild(row);
+    };
+  });
 });
